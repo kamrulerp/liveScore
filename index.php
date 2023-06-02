a4bd52a74c538fca2a5916b193e43cd0c97a532a
unknown <kamrulerp@gmail.com>
2023-06-02 23:19:17 +0600

    Design Home Page using HTML.Bootstrap

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php include('inc/header.php')?>
+        <section class="mt-2">
+            <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="img/1.jpg" class="d-block w-100 custom-height" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/2.jpg" class="d-block w-100 custom-height" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/3.webp" class="d-block w-100 custom-height" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+        </section>    
+
+        <section class="container-fluid pt-5 pb-5">
+            <div class="row">
+                
+                <div class="col-sm-3">
+                    <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-9">
+                                <p class="card-test sm">
+                                    Only test for small screen..
+                                    
+                                </p>
+                            </div>
+                            <div class="col-sm-3">
+                                <span class="badge bg-danger text-right">Test</span>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    IRE  
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    172 & 63-3  
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    ENG  
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    524 & 4-d  
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <small class="text-danger">Day 2 : 3rd session</small>
+                        
+                    </div>
+                    </div>
+                </div>
+
+
+                <div class="col-sm-3">
+                    <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-9">
+                                <p class="card-test sm">
+                                    Only test for small screen..
+                                    
+                                </p>
+                            </div>
+                            <div class="col-sm-3">
+                                <span class="badge bg-danger text-right">Test</span>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    IRE  
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    172 & 63-3  
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    ENG  
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="card-test sm">
+                                    524 & 4-d  
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <small class="text-danger">Day 2 : 3rd session</small>
+                        
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+
+<?php include('inc/footer.php');?>
