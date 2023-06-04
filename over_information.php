2f3c343c5ec7462b1f05f34a383605d196cad118
unknown <chandan285186@gmail.com>
2023-06-04 23:45:34 +0600

    04=06-2023

---
+++ over_information.php
@@ -0,0 +1,77 @@
+<?php include('inc/header.php')?>
+
+
+<div class="container-fulid">
+
+    <div class="container mt-4 mb-4 bg-light">
+        <h3 class="text-center">Over  Information </h3>
+
+    <form>
+  <div class="form-row row">
+    <div class="form-group col-md-6">
+      <label for="full_Name">Over No</label>
+      <input type="text" class="form-control" id="Over_No" placeholder="">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="Nick_Name">Over Bowl</label>
+      <input type="text" class="form-control" id="Over_Bowl" placeholder="">
+    </div>
+	
+   <div class="form-group col-md-6">
+      <label for="Nick_Name">Over Ran</label>
+      <input type="text" class="form-control" id="Over_Ran" placeholder="">
+    </div>
+    
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Over Wicket</label>
+      <input type="text" class="form-control" id="Over_Wicket" placeholder="">
+    </div>
+	
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Bowler Name</label>
+      <input type="text" class="form-control" id="Bowler_Name" placeholder="">
+    </div>
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Bowler Id</label>
+      <input type="text" class="form-control" id="Bowler_Id" placeholder="">
+    </div>
+
+  <div class="form-group col-md-6">
+      <label for="Nick_Name">Status</label>
+      <input type="text" class="form-control" id="Status" placeholder="">
+    </div>
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Entry at</label>
+      <input type="text" class="form-control" id="Entry_at" placeholder="">
+    </div>
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Entry by</label>
+      <input type="text" class="form-control" id="Entry_by" placeholder="">
+    </div>
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Edit at</label>
+      <input type="text" class="form-control" id="Edit_at" placeholder="">
+    </div>
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">Edit by</label>
+      <input type="text" class="form-control" id="edit_by" placeholder="">
+    </div>
+	<div class="form-group col-md-6">
+      <label for="Nick_Name">team name</label>
+      <input type="text" class="form-control" id="team_name" placeholder="">
+    </div>
+	
+	    <div class="pt-2 pb-2" align="center">
+        <button  type="submit" class="btn btn-success d-flax">submit</button>
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+<?php include('inc/footer.php');?>
