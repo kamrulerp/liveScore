81a8102aec28106746d6e9da4e4204a953745faf
unknown <remonsarwar@gmail.com>
2023-06-06 02:08:05 +0600

    Design Completed

--- teams_view.php
+++ teams_view.php
@@ -6,9 +6,68 @@ include('inc/sub_menu.php');
 
 
 
-<div class="container-fulid">
-    <h1>Teams text 2222</h1>
+<div class="container">
 
+    <div class="season_tabs">
+    
+   <div class="season_tab">
+       <input type="radio" id="tab-1" name="tab-group-1" checked>
+       <label for="tab-1">International Cricket Team</label>
+       
+       <div class="season_content">
+            <div class="chip">
+                <img src="img/bd.jpg" alt="Person" width="96" height="96">
+                Bangladesh
+            </div>
+
+            <div class="chip">
+                <img src="img/in.jpg" alt="Person" width="96" height="96">
+                India
+            </div>
+            <div class="chip">
+                <img src="img/bd.jpg" alt="Person" width="96" height="96">
+                Bangladesh
+            </div>
+
+            <div class="chip">
+                <img src="img/in.jpg" alt="Person" width="96" height="96">
+                India
+            </div>
+       </div> 
+   </div>
+    
+   <div class="season_tab">
+       <input type="radio" id="tab-2" name="tab-group-1">
+       <label for="tab-2">Domestic Cricket Team</label>
+       
+       <div class="season_content">
+       <div class="chip">
+                <img src="img/bd.jpg" alt="Person" width="96" height="96">
+                Bangladesh
+            </div>
+
+            <div class="chip">
+                <img src="img/in.jpg" alt="Person" width="96" height="96">
+                India
+            </div>
+       </div> 
+   </div>
+    
+    <div class="season_tab">
+       <input type="radio" id="tab-3" name="tab-group-1">
+       <label for="tab-3">League Cricket Team</label>
+     
+       <div class="season_content">
+
+            <div class="chip">
+                <img src="img/in.jpg" alt="Person" width="96" height="96">
+                India
+            </div>
+       </div> 
+   </div>
+      
+   </div>
+</div>
 
 </div>
 
