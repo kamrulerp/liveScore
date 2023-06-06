499b82bdf4d8d7b46fd08adb36406f6e7629a0d4
unknown <chandan285186@gmail.com>
2023-06-06 23:40:10 +0600

    6-6-2023

--- over_information.php
+++ over_information.php
@@ -1,8 +1,46 @@
 <?php include('inc/header.php')?>
 
+<?php
 
-<div class="container-fulid">
+if(isset($_POST['submit'])){
 
+$over_no = $_POST['over_no'];
+$Over_Bowl = $_POST['Over_Bowl'];
+$Over_Ran = $_POST['Over_Ran'];
+$Over_Wicket = $_POST['Over_Wicket'];
+$Bowler_Name = $_POST['Bowler_Name'];
+$Bowler_Id = $_POST['Bowler_Id'];
+$Status = $_POST['Status'];
+$Entry_at = $_POST['Entry_at'];
+$Entry_by = $_POST['Entry_by'];
+$Edit_at = $_POST['Edit_at'];
+$edit_by = $_POST['edit_by'];
+$team_name = $_POST['team_name'];
+
+$sql = "INSERT INTO `over_info` (`over_no`,`Over_Bowl`,`Over_Ran`,`Over_Wicket`,`Bowler_Name`,`Bowler_Id`,`Status`,`Entry_at`,`Entry_by`,`Edit_at`,`edit_by`) 
+
+VALUES ('".$over_no."','".$Over_Bowl."','".$Over_Ran."','".$Over_Wicket."','".$Bowler_Name."','".$Bowler_Id."','".$Status."','".$Entry_at."','".$Entry_by."',
+  '".$Edit_at."','".$edit_by."','".$team_name."')";
+  
+
+$query =$conn->$query($aql);
+if($query){
+echo"data is insert";
+}
+else{
+echo"data is not insert";
+}
+
+
+
+
+
+}
+
+?>
+ 
+
+   <div class="container-fulid">
     <div class="container mt-4 mb-4 bg-light">
         <h3 class="text-center">Over  Information </h3>
 
@@ -29,7 +67,12 @@
 	
 	<div class="form-group col-md-6">
       <label for="Nick_Name">Bowler Name</label>
-      <input type="text" class="form-control" id="Bowler_Name" placeholder="">
+      <select id="inputState1" class="form-control">
+                <option></option>
+                <option>A</option>
+                <option>B</option>
+				<option>C</option>
+            </select>
     </div>
 	<div class="form-group col-md-6">
       <label for="Nick_Name">Bowler Id</label>
@@ -40,38 +83,21 @@
       <label for="Nick_Name">Status</label>
       <input type="text" class="form-control" id="Status" placeholder="">
     </div>
-	<div class="form-group col-md-6">
-      <label for="Nick_Name">Entry at</label>
-      <input type="text" class="form-control" id="Entry_at" placeholder="">
-    </div>
-	<div class="form-group col-md-6">
-      <label for="Nick_Name">Entry by</label>
-      <input type="text" class="form-control" id="Entry_by" placeholder="">
-    </div>
-	<div class="form-group col-md-6">
-      <label for="Nick_Name">Edit at</label>
-      <input type="text" class="form-control" id="Edit_at" placeholder="">
-    </div>
-	<div class="form-group col-md-6">
-      <label for="Nick_Name">Edit by</label>
-      <input type="text" class="form-control" id="edit_by" placeholder="">
-    </div>
+	
 	<div class="form-group col-md-6">
       <label for="Nick_Name">team name</label>
-      <input type="text" class="form-control" id="team_name" placeholder="">
-    </div>
+       <select id="inputState1" class="form-control">
+                <option></option>
+                <option>A</option>
+                <option>B</option>
+				<option>C</option>
+            </select>
+     </div>
 	
 	    <div class="pt-2 pb-2" align="center">
         <button  type="submit" class="btn btn-success d-flax">submit</button>
-    </div>
-
+      </div>
 </div>
-
-
-
-
-
-
 
 
 <?php include('inc/footer.php');?>
